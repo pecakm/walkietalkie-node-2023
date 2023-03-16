8bb0b0072870821bc9442605b5c3f83509c3da2b
Mikołaj Pęcak <mikolaj.pecak@mug.pl>
2023-03-16 21:42:13 +0100

    Enable socket

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 const express = require('express');
-// const http = require('http');
-// const { Server } = require('socket.io');
-const port = process.env.PORT || 3000;
+const http = require('http');
+const { Server } = require('socket.io');
 
+const port = process.env.PORT || 3001;
 const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: "*",
-//     methods: ["GET", "POST"]
-//   }
-// });
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-// io.on('connection', (socket) => {
-//   socket.on('callUser', (signal) => {
-//     socket.broadcast.emit("incomingCall", signal);
-//   });
+io.on('connection', (socket) => {
+  socket.on('callUser', (signal) => {
+    socket.broadcast.emit("incomingCall", signal);
+  });
 
-//   socket.on('answerCall', (signal) => {
-//     socket.broadcast.emit("callAccepted", signal);
-//   });
+  socket.on('answerCall', (signal) => {
+    socket.broadcast.emit("callAccepted", signal);
+  });
 
-//   socket.on('disconnect', () => {
-//     socket.broadcast.emit('callEnded');
-//   });
-// });
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callEnded');
+  });
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server started at port ${port}`);
 });
