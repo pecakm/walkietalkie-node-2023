298ec4576d323fbd74d572159aea0c7f3b5da57c
Mikołaj Pęcak <mikolaj.pecak@mug.pl>
2023-03-16 19:06:54 +0100

    Add project

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  socket.on('callUser', (signal) => {
+    socket.broadcast.emit("incomingCall", signal);
+  });
+
+  socket.on('answerCall', (signal) => {
+    socket.broadcast.emit("callAccepted", signal);
+  });
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callEnded');
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server started");
+});
