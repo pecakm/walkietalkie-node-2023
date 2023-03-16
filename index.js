2d32eb3e8ab3d2bc2834009193437daa65512dc5
Mikołaj Pęcak <mikolaj.pecak@mug.pl>
2023-03-16 21:19:28 +0100

    Temporary change index

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+// const http = require('http');
+// const { Server } = require('socket.io');
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*",
+//     methods: ["GET", "POST"]
+//   }
+// });
 
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-io.on('connection', (socket) => {
-  socket.on('callUser', (signal) => {
-    socket.broadcast.emit("incomingCall", signal);
-  });
+// io.on('connection', (socket) => {
+//   socket.on('callUser', (signal) => {
+//     socket.broadcast.emit("incomingCall", signal);
+//   });
 
-  socket.on('answerCall', (signal) => {
-    socket.broadcast.emit("callAccepted", signal);
-  });
+//   socket.on('answerCall', (signal) => {
+//     socket.broadcast.emit("callAccepted", signal);
+//   });
 
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('callEnded');
-  });
-});
+//   socket.on('disconnect', () => {
+//     socket.broadcast.emit('callEnded');
+//   });
+// });
 
-server.listen(3001, () => {
+app.listen(3001, () => {
   console.log("Server started");
 });
