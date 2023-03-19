457b5f00dbdd7b0c1372a41d89ed9530509fa454
Mikołaj Pęcak <mikolaj.pecak@mug.pl>
2023-03-19 10:31:44 +0100

    Add mic disabling

--- index.js
+++ index.js
@@ -16,18 +16,32 @@ const io = new Server(server, {
 
 io.on('connection', (socket) => {
   socket.on('callUser', (signal) => {
-    socket.broadcast.emit("incomingCall", signal);
+    socket.broadcast.emit('incomingCall', signal);
   });
 
   socket.on('answerCall', (signal) => {
-    socket.broadcast.emit("callAccepted", signal);
+    socket.broadcast.emit('callAccepted', signal);
+  });
+
+  socket.on('startSpeaking', () => {
+    socket.broadcast.emit('disableMic');
+  });
+
+  socket.on('stopSpeaking', () => {
+    socket.broadcast.emit('enableMic');
   });
 
   socket.on('disconnect', () => {
     socket.broadcast.emit('callEnded');
+    socket.broadcast.emit('userCounter', socket.conn.server.clientsCount);
   });
 
-  socket.emit('turnCredentials', { id: turnId, pwd: turnPwd });
+  io.emit('userCounter', socket.conn.server.clientsCount);
+  socket.emit('initInfo', {
+    initCall: socket.conn.server.clientsCount > 1,
+    turnId,
+    turnPwd,
+  });
 });
 
 server.listen(port, () => {
