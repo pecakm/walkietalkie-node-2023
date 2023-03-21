d2f377e5a8b0b5769b19e2c213bc8977d41c9526
Mikołaj Pęcak <mikolaj.pecak@mug.pl>
2023-03-21 17:40:11 +0100

    Clean code

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ const turnPwd = process.env.TURN_PWD || '';
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
+  cors: { origin: '*', methods: ['GET', 'POST'] },
 });
 
 io.on('connection', (socket) => {
